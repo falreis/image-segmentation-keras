bbbe1030083eabc249cf412db031ed9450a87be2
Felipe Reis <falreis@gmail.com>
2018-07-19 17:27:26 -0300

    Update code

--- code/Models/Segnet.py
+++ code/Models/Segnet.py
@@ -1,10 +1,5 @@
-
-# todo upgrade to keras 2.0
-
-from keras.models import *
-from keras.models import Sequential
-from keras.layers import Reshape
-#from keras.layers import Merge
+from keras.models import Sequential, Model
+from keras.layers import Merge, Input, Reshape
 from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Permute
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Convolution3D, MaxPooling3D, ZeroPadding3D
@@ -15,75 +10,64 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.optimizers import Adam , SGD
 from keras.layers.embeddings import Embedding
 from keras.utils import np_utils
-# from keras.regularizers import ActivityRegularizer
 from keras import backend as K
 
-
 def Segnet(nClasses , optimizer=None , input_height=360, input_width=480 ):
-	kernel = 3
-	filter_size = 64
-	pad = 1
-	pool_size = 2
 
-	model = Sequential()
-	model.add(Layer(input_shape=(3, input_height , input_width )))
+    input_shape=(input_height, input_width, 3)
+    img_input = Input(shape=input_shape)
+    x = img_input
+    
+    # Encoder
+    x = Convolution2D(64, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Convolution2D(128, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Convolution2D(256, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Convolution2D(512, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    # Decoder
+    x = Convolution2D(512, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Convolution2D(256, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Convolution2D(128, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Convolution2D(64, 3, 3, border_mode="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    
+    x = Convolution2D(nClasses, 1, 1, border_mode="valid")(x)
+    x = Reshape((input_shape[0]*input_shape[1], nClasses))(x)
+    x = Activation("softmax")(x)
+    
+    model = Model(img_input, x)
+    
+    model.outputHeight = input_shape[0]
+    model.outputWidth = input_shape[1]
 
-	# encoder
-	model.add(ZeroPadding2D(padding=(pad,pad)))
-	model.add(Convolution2D(filter_size, (kernel, kernel), padding='valid'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(pool_size, pool_size), data_format="channels_first"))
+    if not optimizer is None:
+        model.compile(loss="categorical_crossentropy", optimizer= optimizer , metrics=['accuracy'] )
 
-	model.add(ZeroPadding2D(padding=(pad,pad)))
-	model.add(Convolution2D(128, (kernel, kernel), padding='valid'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(pool_size, pool_size),  data_format="channels_first"))
-
-	model.add(ZeroPadding2D(padding=(pad,pad)))
-	model.add(Convolution2D(256, (kernel, kernel), padding='valid'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
-
-	model.add(ZeroPadding2D(padding=(pad,pad)))
-	model.add(Convolution2D(512, (kernel, kernel), padding='valid'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-
-	# decoder
-	model.add( ZeroPadding2D(padding=(pad,pad)))
-	model.add( Convolution2D(512, (kernel, kernel), padding='valid'))
-	model.add( BatchNormalization())
-
-	model.add( UpSampling2D(size=(pool_size,pool_size)))
-	model.add( ZeroPadding2D(padding=(pad,pad)))
-	model.add( Convolution2D(256, (kernel, kernel), padding='valid'))
-	model.add( BatchNormalization())
-
-	model.add( UpSampling2D(size=(pool_size,pool_size)))
-	model.add( ZeroPadding2D(padding=(pad,pad)))
-	model.add( Convolution2D(128, (kernel, kernel), padding='valid'))
-	model.add( BatchNormalization())
-
-	model.add( UpSampling2D(size=(pool_size,pool_size)))
-	model.add( ZeroPadding2D(padding=(pad,pad)))
-	model.add( Convolution2D(filter_size, (kernel, kernel), padding='valid'))
-	model.add( BatchNormalization())
-
-	model.add(Convolution2D( nClasses , (1, 1), padding='valid',))
-
-	model.outputHeight = model.output_shape[-2]
-	model.outputWidth = model.output_shape[-3]
-
-	model.add(Reshape((nClasses ,  model.outputHeight*model.outputWidth)))
-	
-	model.add(Permute((2, 1)))
-	model.add(Activation('softmax'))
-
-	if not optimizer is None:
-		model.compile(loss="categorical_crossentropy", optimizer= optimizer , metrics=['accuracy'] )
-	
-	return model
-
+    return model
